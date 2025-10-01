06136c9f18a0e361bdea5bb5bc40d04e29870558
BRUNO DAVI DUTKA <bruno_dutka@estudante.sesisenai.org.br>
2025-10-01 17:25:39 -0300

    Conectando ao broker

--- MQTT_basic/MQTT_basic.ino
+++ MQTT_basic/MQTT_basic.ino
@@ -1,11 +1,49 @@
-#
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const String SSID = "iPhone";
+const String PSW = "iot_sul_123";
+
+const String brokerURL = "test.mosquitto.org"; //URL do broker    (servidor)
+const int porta = 1883;                        //Porta do broker  (servidor)
+
+WiFiClient espClient;                 //Criando Cliente WiFi
+PubSubClient mqttClient(espClient);   //Criando Cliente MQTT
+
+void conexaoWifi();
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  conexaoWifi();
+  Serial.println("Conectando ao broker...");
+  mqttClient.setServer(brokerURL.c_str(), porta);
+  String userId = "ESP_BRUNO";
+  mqttClient.connect(userId.c_str());
+  while(!mqttClient.connected()){
+    Serial.println("Erro de conexão");
+    delay(500);
+  }
+  Serial.println("Conectado com sucesso!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("Conexão perdida!!!");
+    conexaoWifi();
+  }
+  mqttClient.loop();
+}
 
+void conexaoWifi(){
+  Serial.println("Iniciando conexão com rede WiFi");
+  WiFi.begin(SSID, PSW);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(400);
+    Serial.print(".");
+    delay(400);
+    Serial.println(".");
+    delay(400);
+  }
+  Serial.println("Rede de Wi-Fi conectada");
 }
