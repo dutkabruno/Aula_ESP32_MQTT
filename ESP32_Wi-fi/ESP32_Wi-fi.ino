c11b7e1b30e2c0d360d59ee7aa4b696e8ac55d91
BRUNO DAVI DUTKA <bruno_dutka@estudante.sesisenai.org.br>
2025-10-01 15:36:20 -0300

    Criação da função para escanear redes locais

--- ESP32_Wi-fi/ESP32_Wi-fi.ino
+++ ESP32_Wi-fi/ESP32_Wi-fi.ino
@@ -1,19 +1,27 @@
 #include <WiFi.h>
 
+void scanLocalNetworks();
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Iniciando Scan de redes Wi-Fi");
+  scanLocalNetworks();
 }
 
 void loop() {
+  
+}
+
+
+void scanLocalNetworks(){
   int number = WiFi.scanNetworks();
-  if(number == -1){
-    Serial.println("ERRO!. Não deu bom");
-  }
-  else{
-    for(int net = 0; net < number; net++){
-      Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
+    if(number == -1){
+      Serial.println("ERRO!. Não deu bom");
     }
-    //Serial.printf("O número de redes encontradas é de: %d\n", number);
-  }
+    else{
+      for(int net = 0; net < number; net++){
+        Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
+      }
+      //Serial.printf("O número de redes encontradas é de: %d\n", number);
+    }
 }
