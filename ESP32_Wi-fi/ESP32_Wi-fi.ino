05d16c9b0df9bbac767c9b284038f3267598a709
BRUNO DAVI DUTKA <bruno_dutka@estudante.sesisenai.org.br>
2025-10-01 15:24:56 -0300

    Testando as redes WI-Fi

--- ESP32_Wi-fi/ESP32_Wi-fi.ino
+++ ESP32_Wi-fi/ESP32_Wi-fi.ino
@@ -1,11 +1,19 @@
-#define <Wi-Fi.h>
+#include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("Iniciando Scan de redes Wi-Fi");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int number = WiFi.scanNetworks();
+  if(number == -1){
+    Serial.println("ERRO!. Não deu bom");
+  }
+  else{
+    for(int net = 0; net < number; net++){
+      Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
+    }
+    //Serial.printf("O número de redes encontradas é de: %d\n", number);
+  }
 }
